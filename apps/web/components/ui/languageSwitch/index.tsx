0c629f52ac01f7483aec137181ea8adafe4aa20e
alex freitas <alexfreitasbr@hotmail.com>
2026-06-24 00:48:29 -0300

    flags

--- apps/web/components/ui/languageSwitch/index.tsx
+++ apps/web/components/ui/languageSwitch/index.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { useLanguageStore } from "@/stores/language";
+import { BR, US } from 'country-flag-icons/react/3x2';
 
 export function LanguageSwitcher() {
-  const { locale, setLocale, language } = useLanguageStore();
+  const { locale, setLocale} = useLanguageStore();
   return (
     <button onClick={() => setLocale(locale === "pt" ? "en" : "pt")}>
-      {language.language}
+      {locale === 'pt' ? <BR title="Brasil" className="w-6 h-6" /> : <US title="Estados Unidos" className="w-6 h-6" />} 
     </button>
   );
 }
